631f86f788aaab767c52301a45df15a508993dad
andredassa <andredassa@hotmail.com>
2015-06-10 12:45:45 -0300

    Autor e título

---
+++ NovinhaNoGrau.NoFluxo/NovinhaNoGrau.NoFluxo.API/Models/Autor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NovinhaNoGrau.NoFluxo.API.Models
+{
+	public class Autor
+	{
+		public int Id { get; set; }
+		public string NomeAutor { get; set; }
+		public List<Titulo> Titulos { get; set; }
+	}
+}
---
+++ NovinhaNoGrau.NoFluxo/NovinhaNoGrau.NoFluxo.API/Models/Titulo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NovinhaNoGrau.NoFluxo.API.Models
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public class Titulo
+	{
+	}
+}
