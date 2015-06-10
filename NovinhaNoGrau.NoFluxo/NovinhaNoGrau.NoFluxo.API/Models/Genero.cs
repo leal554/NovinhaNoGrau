5778e5abf7590652062e980a28be3c691a1da991
leal554 <leal_554@msn.com>
2015-06-10 12:22:18 -0300

    novas classes

---
+++ NovinhaNoGrau.NoFluxo/NovinhaNoGrau.NoFluxo.API/Models/Genero.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NovinhaNoGrau.NoFluxo.API.Models
+{
+	public class Genero
+	{
+		public int id { get; set; }
+		public string nome { get; set; }
+		public int GeneroID { get; set; }
+		public Genero GeneroPai { get; set; }
+
+	}
+}
